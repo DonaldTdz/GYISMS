49294c3f4ec2577b942f90c36a996dd3a2b9d0f6
Donald <tangdezou@qq.com>
2018-09-29 18:31:34 +0800

    区县报表

---
+++ aspnet-core/src/GYISMS.Application/Charts/Dtos/ScheduleSummaryDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GYISMS.Charts.Dtos
+{
+    public class ScheduleSummaryDto
+    {
+        public ScheduleSummaryDto()
+        {
+            A = "1";
+        }
+        /// <summary>
+        /// 统计名称 完成、进行中、逾期
+        /// </summary>
+        public string Name { get; set; }
+
+        public int? Num { get; set; }
+
+        public decimal? Percent { get; set; }
+
+        public string A { get; set; }
+
+        public string ClassName { get; set; }
+
+        public int Seq { get; set; }
+    }
+}
